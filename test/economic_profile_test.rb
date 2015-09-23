dcaa86ac0b032f9af0191a51c4a7007d8b367dda
PenneyGadget <dreamofgreen@gmail.com>
2015-09-22 18:53:11 -0600

    wrote economic profile tests

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -49,5 +49,92 @@ class EconomicProfileTest < Minitest::Test
     assert_equal expected, @economic_profile.free_or_reduced_lunch_in_year(2030)
   end
 
+  def test_free_or_reduced_lunch_in_year_method_returns_three_digit_percentage_float
+    skip
+    #typo in spec
+  end
+
+  def test_school_aged_children_in_poverty_by_year_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_floats
+    skip
+    expected = { 1995 => 0.032,
+                 1997 => 0.035,
+                 1999 => 0.032,
+                 2000 => 0.031,
+                 2001 => 0.029,
+                 2002 => 0.033,
+                 2003 => 0.037,
+                 2004 => 0.034,
+                 2005 => 0.042,
+                 2006 => 0.036,
+                 2007 => 0.039,
+                 2008 => 0.044,
+                 2009 => 0.047,
+                 2010 => 0.057,
+                 2011 => 0.059,
+                 2012 => 0.064,
+                 2013 => 0.048,
+               }
+
+    assert_equal expected, @economic_profile.school_aged_children_in_poverty_in_year
+  end
+
+  def test_school_aged_children_in_poverty_by_year_method_returns_an_empty_hash_if_district_data_is_not_present
+    skip
+    expected = []
+
+    assert_equal expected, ? not sure what goes here yet
+  end
+
+  def test_school_aged_children_in_poverty_in_year_method_takes_valid_parameter
+    skip
+    #takes year as integer
+  end
+
+  def test_school_aged_children_in_poverty_in_year_method_returns_nil_for_any_unknown_year
+    skip
+    expected = nil
+
+    assert_equal expected, @economic_profile.school_aged_children_in_poverty_in_year(1111)
+  end
+
+  def test_school_aged_children_in_poverty_in_year_method_returns_three_digit_percentage_float
+    skip
+    expected = 0.064
+
+    assert_equal expected, @economic_profile.school_aged_children_in_poverty_in_year(2012)
+  end
+
+  def test_title_1_students_by_year_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_floats
+    skip
+    expected = {2009 => 0.014, 2011 => 0.011, 2012 => 0.01, 2013 => 0.012, 2014 => 0.027}
+
+    assert_equal expected, @economic_profile.title_1_students_by_year
+  end
+
+  def test_title_1_students_by_year_method_returns_an_empty_hash_if_the_district_data_is_not_present
+    skip
+    expected = []
+
+    assert_equal expected, ? not sure what goes here yet
+  end
+
+  def test_title_1_students_in_year_method_takes_valid_parameter
+    skip
+    #takes year as an integer
+  end
+
+  def test_title_1_students_in_year_method_returns_nil_for_any_unknown_year
+    skip
+    expected = nil
+
+    assert_equal expected, @economic_profile.title_1_students_in_year(3333)
+  end
+
+  def test_title_1_students_in_year_method_returns_three_digit_percentage_float
+    skip
+    expected = 0.01
+
+    assert_equal expected, @economic_profile.title_1_students_in_year(2012)
+  end
 
 end
