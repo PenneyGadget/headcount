92448ee7379fe766de318e6da49dc01734c1a778
PenneyGadget <dreamofgreen@gmail.com>
2015-09-29 21:04:58 -0600

    closing in on finishing the enrollment class

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,7 +1,10 @@
 require 'pry'
 
+class UnknownRaceError < StandardError
+end
+
 class Enrollment
-  attr_accessor :district_data, :enrollment_data, :race_table
+  attr_accessor :district_data, :enrollment_data, :race_table, :race_table_two
 
   def initialize(district_data)
     @district_data = district_data
@@ -21,6 +24,14 @@ class Enrollment
                    "Native American Students" => :native_american,
                    "Two or More Races" => :two_or_more,
                    "White Students" => :white}
+
+    @race_table_two = {"Asian Students" => :asian,
+                   "Black Students" => :black,
+                   "Native Hawaiian or Other Pacific Islander" => :pacific_islander,
+                   "Hispanic Students" => :hispanic,
+                   "American Indian Students" => :native_american,
+                   "Two or more races" => :two_or_more,
+                   "White Students" => :white}
   end
 
   def truncate(number)
@@ -33,6 +44,10 @@ class Enrollment
     years.include? year
   end
 
+  def valid_race?(race)
+    @race_table.values.include?(race)
+  end
+
   # receives integer, returns percentage of all students for a specific district
   def dropout_rate_in_year(year)
     if valid_year?(year)
@@ -43,77 +58,137 @@ class Enrollment
     end
   end
 
-  # receives integer, returns gender/percentage hash (how is district being addressed without hard coding?)
+  # receives integer, returns gender/percentage hash
   def dropout_rate_by_gender_in_year(year)
-    female = @dropout_rate.find_all { |hash| hash[:category] == "Female Students" && hash[:timeframe] == "#{year}" && hash[:dataformat] == "Percent" }
-    male = @dropout_rate.find_all { |hash| hash[:category] == "Male Students" && hash[:timeframe] == "#{year}" && hash[:dataformat] == "Percent" }
-    data = {female: truncate(female[0][:data]), male: truncate(male[0][:data])}
+    if valid_year?(year)
+      female = @dropout_rate.find_all { |hash| hash[:category] == "Female Students" && hash[:timeframe] == "#{year}" && hash[:dataformat] == "Percent" }
+      male = @dropout_rate.find_all { |hash| hash[:category] == "Male Students" && hash[:timeframe] == "#{year}" && hash[:dataformat] == "Percent" }
+      data = {female: truncate(female[0][:data]), male: truncate(male[0][:data])}
+    else
+      nil
+    end
   end
 
   # receives integer, returns race/percentage hash
   def dropout_rate_by_race_in_year(year)
-    dropout_rate_select_data = @dropout_rate.find_all { |hash| hash[:category] != "All Students" && hash[:category] != "Female Students" && hash[:category] != "Male Students" && hash[:timeframe] == "#{year}" }
-    race = dropout_rate_select_data.map { |hash| hash.fetch(:category) }
-    data = dropout_rate_select_data.map { |hash| hash.fetch(:data) }
-    dropout_rate = Hash[race.zip(data.map { |num| truncate(num) })]
-    final_hash = Hash[dropout_rate.map { |k, v| [race_table[k], v] }]
+    if valid_year?(year)
+      dropout_rate_select_data = @dropout_rate.find_all { |hash| hash[:category] != "All Students" && hash[:category] != "Female Students" && hash[:category] != "Male Students" && hash[:timeframe] == "#{year}" }
+      race = dropout_rate_select_data.map { |hash| hash.fetch(:category) }
+      data = dropout_rate_select_data.map { |hash| hash.fetch(:data) }
+      dropout_rate = Hash[race.zip(data.map { |num| truncate(num) })]
+      Hash[dropout_rate.map { |k, v| [race_table[k], v] }]
+    else
+      nil
+    end
   end
 
   # receives symbol, returns year/percentage hash
   def dropout_rate_for_race_or_ethnicity(race)
-    category = @race_table.select {|k, v| k if v == race  }.keys
+    raise UnknownRaceError unless valid_race?(race)
+    category = @race_table.select { |k, v| k if v == race }.keys
     @dropout_rate.select do |row|
       row[:category] == category.first
     end.map { |row| [row[:timeframe].to_i, truncate(row[:data])] }.to_h
   end
 
+  # receives symbol and integer, returns float
   def dropout_rate_for_race_or_ethnicity_in_year(race, year)
-
+    raise UnknownRaceError unless valid_race?(race)
+      if valid_year?(year)
+        dropout_rate_for_race_or_ethnicity(race)[year]
+      else
+        nil
+      end
   end
 
+  # returns a year/percentage hash
   def graduation_rate_by_year
-
+    year = @graduation_rate.map { |hash| hash.fetch(:timeframe).to_i }
+    data = @graduation_rate.map { |hash| hash.fetch(:data) }
+    Hash[year.zip(data.map { |num| truncate(num) })]
   end
 
-  # receives integer, returns percentage of all students for a specific district
+  # receives integer, returns float
   def graduation_rate_in_year(year)
-    graduation_rate_select_data = @graduation_rate.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
-    truncate(graduation_rate_select_data[0][:data])
+    if valid_year?(year)
+      graduation_rate_select_data = @graduation_rate.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
+      truncate(graduation_rate_select_data[0][:data])
+    else
+      nil
+    end
   end
 
+  # returns a year/percentage hash
   def kindergarten_participation_by_year
-
+    year = @kindergartners.map { |hash| hash.fetch(:timeframe).to_i }
+    data = @kindergartners.map { |hash| hash.fetch(:data) }
+    Hash[year.zip(data.map { |num| truncate(num) })]
   end
 
+  # receives integer, returns float
   def kindergarten_participation_in_year(year)
-    kindergarten_participation_select_data = @kindergartners.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
-    truncate(kindergarten_participation_select_data[0][:data])
+    if valid_year?(year)
+      kindergarten_participation_select_data = @kindergartners.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
+      truncate(kindergarten_participation_select_data[0][:data])
+    else
+      nil
+    end
   end
 
+  # returns a year/percentage hash
   def online_participation_by_year
-
+    year = @online_enrollment.map { |hash| hash.fetch(:timeframe).to_i }
+    data = @online_enrollment.map { |hash| hash.fetch(:data) }
+    Hash[year.zip(data.map { |num| truncate(num) })]
   end
 
+  # receives integer, returns integer
   def online_participation_in_year(year)
-    online_enrollment_select_data = @online_enrollment.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
-    truncate(online_enrollment_select_data[0][:data])
+    if valid_year?(year)
+      online_enrollment_select_data = @online_enrollment.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
+      truncate(online_enrollment_select_data[0][:data])
+    else
+      nil
+    end
   end
 
+  # returns a year/integer hash
   def participation_by_year
-
+    year = @pupil_enrollment.map { |hash| hash.fetch(:timeframe).to_i }
+    data = @pupil_enrollment.map { |hash| hash.fetch(:data) }
+    Hash[year.zip(data.map { |num| truncate(num) })]
   end
 
+  # receives integer, returns integer
   def participation_in_year(year)
-    pupil_enrollment_select_data = @pupil_enrollment.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
-    truncate(pupil_enrollment_select_data[0][:data])
+    if valid_year?(year)
+      pupil_enrollment_select_data = @pupil_enrollment.find_all { |hash| hash[:timeframe] == "#{year}" && hash[:data] }
+      truncate(pupil_enrollment_select_data[0][:data])
+    else
+      nil
+    end
   end
 
-  def participation_by_race_or_ethinicity(race)
-
+  # receives symbol, returns year/percentage hash
+  def participation_by_race_or_ethnicity(race)
+    raise UnknownRaceError unless valid_race?(race)
+    category = @race_table.select { |k, v| k if v == race }.keys
+    @pupil_enrollment_race.select do |row|
+      row[:race] == category.first
+    end.map { |row| [row[:timeframe].to_i, truncate(row[:data])] }.to_h
   end
 
-  def participation_by_race_or_ethinicity_in_year(year)
-
+  # receives integer, returns symbol/percentage hash
+  def participation_by_race_or_ethnicity_in_year(year)
+    if valid_year?(year)
+      pupil_race_select_data = @pupil_enrollment_race.find_all { |hash| hash[:race] != "All Students" && hash[:race] != "Total" && hash[:category] != "Female Students" && hash[:category] != "Male Students" && hash[:dataformat] == "Percent" && hash[:timeframe] == "#{year}" }
+      race = pupil_race_select_data.map { |hash| hash.fetch(:race) }
+      data = pupil_race_select_data.map { |hash| hash.fetch(:data) }
+      participation_by_race = Hash[race.zip(data.map { |num| truncate(num) })]
+      Hash[participation_by_race.map { |k, v| [race_table_two[k], v] }]
+    else
+      nil
+    end
   end
 
   def special_education_by_year
--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -24,7 +24,6 @@ class EnrollmentTest < Minitest::Test
   end
 
   def test_dropout_rate_by_gender_in_year_method_returns_nil_for_unknown_years
-    skip
     assert_equal nil, @district.enrollment.dropout_rate_by_gender_in_year(232323322332)
 
     assert_equal nil, @district.enrollment.dropout_rate_by_gender_in_year(1980)
@@ -36,12 +35,11 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, @district.enrollment.dropout_rate_by_gender_in_year(2011)
   end
 
-  # def test_dropout_rate_by_race_in_year_method_returns_nil_with_any_unknown_year
-  #   skip
-  #   assert_equal nil, @district.enrollment.dropout_rate_by_race_in_year(36485968)
-  #
-  #   assert_equal nil, @district.enrollment.dropout_rate_by_race_in_year(1980)
-  # end
+  def test_dropout_rate_by_race_in_year_method_returns_nil_with_any_unknown_year
+    assert_equal nil, @district.enrollment.dropout_rate_by_race_in_year(36485968)
+
+    assert_equal nil, @district.enrollment.dropout_rate_by_race_in_year(1980)
+  end
 
   def test_dropout_rate_by_race_in_year_method_returns_hash_with_race_as_keys_and_three_digit_percentage_floats
     expected = { asian: 0.007,
@@ -56,12 +54,11 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, @district.enrollment.dropout_rate_by_race_in_year(2012)
   end
 
-  # def test_dropout_rate_for_race_or_ethnicity_method_raises_an_error_with_unknown_race
-  #   skip
-  #   assert_raises UnknownRaceError do
-  #     @district.enrollment.dropout_rate_for_race_or_ethnicity(:german)
-  #   end
-  # end
+  def test_dropout_rate_for_race_or_ethnicity_method_raises_an_error_with_unknown_race
+    assert_raises UnknownRaceError do
+      @district.enrollment.dropout_rate_for_race_or_ethnicity(:german)
+    end
+  end
 
   def test_dropout_rate_for_race_or_ethnicity_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_float
     expected = {2011 => 0.000, 2012 => 0.007}
@@ -69,40 +66,32 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, @district.enrollment.dropout_rate_for_race_or_ethnicity(:asian)
   end
 
-  # def test_dropout_rate_for_race_or_ethnicity_in_year_returns_nil_with_any_unknown_year
-  #   skip
-  #   expected = nil
-  #
-  #   assert_equal expected, @district.enrollment.dropout_rate_for_race_or_ethnicity_in_year(:asian, 1940)
-  #
-  #   assert_equal expected, @district.enrollment.dropout_rate_for_race_or_ethnicity_in_year(:white, 666)
-  # end
-  #
-  # def test_dropout_rate_for_race_or_ethnicity_in_year_returns_three_digit_percentage_float
-  #   skip
-  #   expected = 0.001
-  #
-  #   assert_equal expected, @district.enrollment.dropout_rate_for_race_or_ethnicity_in_year(:asian, 2012)
-  # end
-  #
-  # def test_graduation_rate_by_year_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_float
-  #   skip
-  #   expected = { 2010 => 0.895,
-  #                2011 => 0.895,
-  #                2012 => 0.889,
-  #                2013 => 0.913,
-  #                2014 => 0.898,
-  #              }
-  #
-  #   assert_equal expected, @district.enrollment.graduation_rate_by_year
-  # end
+  def test_dropout_rate_for_race_or_ethnicity_in_year_returns_nil_with_any_unknown_year
+    assert_equal nil, @district.enrollment.dropout_rate_for_race_or_ethnicity_in_year(:asian, 1940)
 
-  # def test_graduation_rate_in_year_method_returns_nil_with_any_unknown_year
-  #   skip
-  #   expected = nil
-  #
-  #   assert_equal expected, @district.enrollment.graduation_rate_in_year(1937)
-  # end
+    assert_equal nil, @district.enrollment.dropout_rate_for_race_or_ethnicity_in_year(:white, 666)
+  end
+
+  def test_dropout_rate_for_race_or_ethnicity_in_year_returns_three_digit_percentage_float
+    expected = 0.007
+
+    assert_equal expected, @district.enrollment.dropout_rate_for_race_or_ethnicity_in_year(:asian, 2012)
+  end
+
+  def test_graduation_rate_by_year_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_float
+    expected = { 2010 => 0.895,
+                 2011 => 0.895,
+                 2012 => 0.889,
+                 2013 => 0.913,
+                 2014 => 0.898,
+               }
+
+    assert_equal expected, @district.enrollment.graduation_rate_by_year
+  end
+
+  def test_graduation_rate_in_year_method_returns_nil_with_any_unknown_year
+    assert_equal nil, @district.enrollment.graduation_rate_in_year(1937)
+  end
 
   def test_graduation_rate_in_year_method_returns_three_digit_percentage_float
     expected = 0.895
@@ -110,24 +99,27 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, @district.enrollment.graduation_rate_in_year(2010)
   end
 
-  # def test_kindergarten_participation_by_year_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_floats
-  #   skip
-  #   expected = { 2010 => 0.391,
-  #                2011 => 0.353,
-  #                2012 => 0.267,
-  #                2013 => 0.487,
-  #                2014 => 0.490,
-  #              }
-  #
-  #   assert_equal expected, @district.enrollment.kindergarten_participation_by_year
-  # end
+  def test_kindergarten_participation_by_year_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_floats
+    expected = { 2007 => 0.391,
+                 2006 => 0.353,
+                 2005 => 0.267,
+                 2004 => 0.302,
+                 2008 => 0.384,
+                 2009 => 0.39,
+                 2010 => 0.436,
+                 2011 => 0.489,
+                 2012 => 0.478,
+                 2013 => 0.487,
+                 2014 => 0.490
+               }
 
-  # def test_kindergarten_participation_in_year_method_returns_nil_with_any_unknown_year
-  #   skip
-  #   expected = nil
-  #
-  #   assert_equal expected, @district.enrollment.kindergarten_participation_in_year(1999)
-  # end
+    assert_equal expected, @district.enrollment.kindergarten_participation_by_year
+  end
+
+  def test_kindergarten_participation_in_year_method_returns_nil_with_any_unknown_year
+    skip
+    assert_equal nil, @district.enrollment.kindergarten_participation_in_year(1999)
+  end
 
   def test_kindergarten_participation_in_year_method_returns_three_digit_percentage_float
     expected = 0.436
@@ -135,24 +127,18 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, @district.enrollment.kindergarten_participation_in_year(2010)
   end
 
-  # def test_online_participation_by_year_method_returns_a_hash_with_years_as_keys_and_an_integer_as_value
-  #   skip
-  #   expected = { 2010 => 16,
-  #                2011 => 18,
-  #                2012 => 24,
-  #                2013 => 32,
-  #                2014 => 40,
-  #              }
-  #
-  #   assert_equal expected, @district.enrollment.online_participation_by_year
-  # end
+  def test_online_participation_by_year_method_returns_a_hash_with_years_as_keys_and_an_integer_as_value
+    expected = { 2011 => 33.0,
+                 2012 => 35.0,
+                 2013 => 341.0
+               }
 
-  # def test_online_participation_in_year_method_returns_nil_with_any_unknown_year
-  #   skip
-  #   expected = nil
-  #
-  #   assert_equal nil, @district.enrollment.online_participation_in_year(1990)
-  # end
+    assert_equal expected, @district.enrollment.online_participation_by_year
+  end
+
+  def test_online_participation_in_year_method_returns_nil_with_any_unknown_year
+    assert_equal nil, @district.enrollment.online_participation_in_year(1990)
+  end
 
   def test_online_participation_in_year_method_returns_a_single_integer
     expected = 341
@@ -160,25 +146,21 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, @district.enrollment.online_participation_in_year(2013)
   end
 
-  # def test_participation_by_year_method_returns_a_hash_with_years_as_keys_and_an_integer_as_value
-  #   skip
-  #   expected = { 2009 => 22620,
-  #                2010 => 22620,
-  #                2011 => 23119,
-  #                2012 => 23657,
-  #                2013 => 23973,
-  #                2014 => 24578,
-  #              }
-  #
-  #   assert_equal expected, @district.enrollment.participation_by_year
-  # end
+  def test_participation_by_year_method_returns_a_hash_with_years_as_keys_and_an_integer_as_value
+    expected = { 2009 => 22620.0,
+                 2010 => 23119.0,
+                 2011 => 23657.0,
+                 2012 => 23973.0,
+                 2013 => 24481.0,
+                 2014 => 24578.0
+               }
 
-  # def test_participation_in_year_method_returns_nil_with_any_unknown_year
-  #   skip
-  #   expected = nil
-  #
-  #   assert_equal expected, @district.enrollment.participation_in_year(1650)
-  # end
+    assert_equal expected, @district.enrollment.participation_by_year
+  end
+
+  def test_participation_in_year_method_returns_nil_with_any_unknown_year
+    assert_equal nil, @district.enrollment.participation_in_year(1650)
+  end
 
   def test_participation_in_year_method_returns_returns_a_single_integer
     expected = 24481
@@ -186,43 +168,43 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, @district.enrollment.participation_in_year(2013)
   end
 
-  # def test_participation_by_race_or_ethnicity_raises_an_error_with_any_unknown_race
-  #   skip
-  #   assert_raises UnknownRaceError do
-  #     @district.enrollment.participation_by_race_or_ethinicity(:nepalese)
-  #   end
-  # end
-  #
-  # def test_participation_by_race_or_ethnicity_returns_a_hash_with_years_as_keys_and_three_digit_percentage_float
-  #   skip
-  #   expected = { 2011 => 0.047,
-  #                2012 => 0.041,
-  #                2013 => 0.052,
-  #                2014 => 0.056
-  #              }
-  #
-  #   assert_equal expected, @district.enrollment.participation_by_race_or_ethinicity(:asian)
-  # end
+  def test_participation_by_race_or_ethnicity_raises_an_error_with_any_unknown_race
+    assert_raises UnknownRaceError do
+      @district.enrollment.participation_by_race_or_ethnicity(:nepalese)
+    end
+  end
 
-  # def test_participation_by_race_or_ethnicity_in_year_method_returns_nil_with_any_unknown_year
-  #   skip
-  #   expected = nil
-  #
-  #   assert_equal expected, @district.enrollment.participation_by_race_or_ethinicity_in_year(1900)
-  # end
-  #
-  # def test_participation_by_race_or_ethnicity_in_year_method_returns_a_hash_with_race_as_keys_and_three_digit_percentage_float
-  #   expected =  { :asian => 0.036,
-  #                 :black => 0.029,
-  #                 :pacific_islander => 0.118,
-  #                 :hispanic => 0.003,
-  #                 :native_american => 0.004,
-  #                 :two_or_more => 0.050,
-  #                 :white => 0.756
-  #                }
-  #
-  #   assert_equal expected, @district.enrollment.participation_by_race_or_ethinicity_in_year(2012)
-  # end
+  def test_participation_by_race_or_ethnicity_returns_a_hash_with_years_as_keys_and_three_digit_percentage_float
+    expected = { 2007 => 0.05,
+                 2008 => 0.054,
+                 2009 => 0.055,
+                 2010 => 0.04,
+                 2011 => 0.036,
+                 2012 => 914.0,
+                 2013 => 932.0,
+                 2014 => 933.0
+               }
+
+    assert_equal expected, @district.enrollment.participation_by_race_or_ethnicity(:asian)
+  end
+
+  def test_participation_by_race_or_ethnicity_in_year_method_returns_nil_with_any_unknown_year
+    skip
+    assert_equal nil, @district.enrollment.participation_by_race_or_ethinicity_in_year(1900)
+  end
+
+  def test_participation_by_race_or_ethnicity_in_year_method_returns_a_hash_with_race_as_keys_and_three_digit_percentage_float
+    expected =  { native_american:  0.004,
+                  asian:            0.038,
+                  black:            0.031,
+                  hispanic:         0.121,
+                  pacific_islander: 0.004,
+                  two_or_more:      0.053,
+                  white:            0.750,
+                 }
+
+    assert_equal expected, @district.enrollment.participation_by_race_or_ethnicity_in_year(2012)
+  end
 
   # def test_special_education_by_year_method_returns_a_hash_with_years_as_keys_and_three_digit_percentage_floats
   #   skip
